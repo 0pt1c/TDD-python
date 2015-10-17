7dd329e1d60753eff128cdeb26a3891a5b26e0b2
Chris Bryant <chris@urbandiscourse.com>
2015-10-17 11:33:55 -0500

    add check_for_row_in_list_table helper function

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id(
+            'id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Edith has heard about a cool new online to-do app
@@ -31,20 +37,23 @@ class NewVisitorTest(unittest.TestCase):
                          'Enter a to-do item')
         # She types "Buy peacock feathers" into a text box
         # Her hobby is tying fly lures
-        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page
         # lists her to-do item
         inputbox.send_keys(Keys.ENTER)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box to enter another item
         # She enters "Use feather to make a fly"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feather to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feather to make a fly')
 
         # The page updates again and shows both items
 
@@ -55,6 +64,8 @@ class NewVisitorTest(unittest.TestCase):
         # She visits the URL - her to-do list is still there
 
         # Satisfied, she goes back to sleep
+
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
